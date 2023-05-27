9ee5f1e01386ec6dab2447a94ce13e5a2bbb662a
Carlos Alexandre Carmona da Silva <carlosalexandress340@gmail.com>
2023-05-27 08:27:11 -0400

    Visual

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProjetoClima
  Projeto de pesquisa de clima, com integração da API OpenWeather.
+key 5084f333b4ea1034622cd1536f9bd293
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+/*API key 5084f333b4ea1034622cd1536f9bd293*/
+
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={5084f333b4ea1034622cd1536f9bd293}
